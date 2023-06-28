f7c0a1a6979729e6bfecfb3c183332ce054424f1
Matheus oliveira <matheusoliveiradesouza190503@gmail.com>
2023-06-28 17:33:37 -0300

    routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,27 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-use App\Http\Controllers\cadastro_login_controller;
-use App\Http\Controllers\viewscontroller;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-//tela inicial
-Route::get('/', [viewscontroller::class, 'tela_index'])->name('index');
-
-
-//tela de cadastro
-=======
 use App\Http\Controllers\{
     cadastro_login_controller,
     viewscontroller,
@@ -30,17 +9,12 @@ use App\Http\Controllers\{
 };
 
 //Route Controlar Cadastro
->>>>>>> e0651baf25f08f391684f0ad3629b8432296fccc
 route::prefix('cadastro')->group(function(){
     Route::get('/', [viewscontroller::class,'tela_cadastro'])->name('tela_cadastro');
     Route::post('/store', [cadastro_login_controller::class,'cadastro'])->name('cadastro-store');
 });
 
-<<<<<<< HEAD
 //tela de login
-=======
-//Route Controlar Login
->>>>>>> e0651baf25f08f391684f0ad3629b8432296fccc
 route::prefix('login')->group(function(){
     Route::get('/',[viewscontroller::class,'tela_login'])->name('tela_login');
     Route::post('/store', [cadastro_login_controller::class,'login'])->name('login-store');
