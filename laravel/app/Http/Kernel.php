0412e29cf4b6af155a48e1e6b942d2b27bcfe428
Matheus oliveira <matheusoliveiradesouza190503@gmail.com>
2023-06-28 19:16:57 -0300

    Rotas middleware

--- laravel/app/Http/Kernel.php
+++ laravel/app/Http/Kernel.php
@@ -63,5 +63,9 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'cliente' => \App\Http\Middleware\verificacliente::class,
+        'atendente' => \App\Http\Middleware\verificaatendente::class,
+        'administrador' => \App\Http\Middleware\verificaadministrador::class,
+        'logado' => \App\Http\Middleware\verificalogado::class,
     ];
 }
