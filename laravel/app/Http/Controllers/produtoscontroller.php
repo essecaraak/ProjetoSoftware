c4bc534fe40fd12fc1cc6d3777ed1f2c4fc35ef6
Matheus oliveira <matheusoliveiradesouza190503@gmail.com>
2023-07-04 23:05:43 -0300

    rota nova

--- laravel/app/Http/Controllers/produtoscontroller.php
+++ laravel/app/Http/Controllers/produtoscontroller.php
@@ -32,4 +32,27 @@ class produtoscontroller extends Controller
        $produto->save();
        
     }
+    public function produtos_update(produtorequest $request, $id){
+        
+        $produto =new  produto();
+        $produto->nome = $request->nome;
+        $produto->tipo = $request->tipo;
+        if(!is_null($request->descricao)){
+            $produto->descricao = $request->descricao;
+        }
+        
+        $produto->quantidade = $request->quantidade;
+        $produto->valor = $request->valor;
+        $filename =$request->file('imagem')->getClientOriginalName();
+        
+       if($request->hasFile('imagem')){
+            if($request->file('imagem')->isValid()){
+                $path =$request->file('imagem')->storeAs('produtos',$filename,'public');
+                
+                $produto->imagem = '/storage/'.$path;
+            }
+       }
+       $produto->save();
+       
+    }
 }
